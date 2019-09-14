49086fff3243f842109280d7f40c6520bcd6556e
Raimundo Herrera <rjherrera@uc.cl>
2019-09-13 22:09:12 -0300

    feat(T1): add traces solution (P1)

---
+++ T1/traces.py
@@ -0,0 +1,18 @@
+def traces(n, heights):
+    if n == 0 or heights.count(0) == n:
+        return 0
+    m = min(heights)
+    ix = heights.index(m)
+    new = [i - m for i in heights]
+    return min(n, m + traces(ix, new[:ix]) + traces(n - ix - 1, new[ix + 1:]))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    elements = [int(i) for i in input().split()]
+
+    # input_text = '7\n2 4 3 6 4 2 1'
+    # inputs = input_text.split('\n')
+    # n, elements = int(inputs[0]), [int(i) for i in inputs[1].split()]
+
+    print(traces(n, elements))
